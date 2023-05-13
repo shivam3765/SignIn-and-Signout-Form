940100910ceeaa7ff68872f2c942e0f5ed369af5
vampi <vampi3765@gmail.com>
2023-05-14 02:15:50 +0530

    set form validation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
+require('dotenv').config();  
 const express = require("express");
 const app = express();
 const path = require("path");
-const collection = require("./mongodb")
+const collection = require("./mongodb");
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
+const e = require('express');
+
 
 const port = process.env.PORT || 8080;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates");
 
-app.use(express.json());
-app.use(express.urlencoded({ extented: false }));
+app.use(bodyParser.urlencoded({ extented: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(static_path));
 
@@ -23,7 +27,14 @@ app.get("/", (req, res) => {
 });
 
 
-app.post("/registration", async (req, res) => {
+app.post("/registration", [
+    check('firstName').exists().isLength({ min: 3 }).withMessage('firstName length should be more then 3 characters'),
+    check('lastName').exists().isLength({ min: 3 }).withMessage('lastName length should be morethen 3 characters'),
+    check('Email').isEmail().normalizeEmail().withMessage('Email is not valid'),
+    check('Password').isStrongPassword().isLength({ min: 6 }).withMessage( "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long"),
+    check('Phone').isLength({ min: 10, max: 10 }).withMessage('Phone number should contains 10 digits')
+    
+], async (req, res) => {
     try {
 
         const data = {
@@ -35,8 +46,16 @@ app.post("/registration", async (req, res) => {
             Gender: req.body.Radio
         }
 
-        await collection.insertMany([data])
-        res.status(201).render("registration");
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.send("Invalid Details.....")
+
+        }
+        else {
+            await collection.insertMany([data])
+            res.status(201).render("registration")
+            
+        }
 
     } catch (error) {
         res.status(400).send(error);
